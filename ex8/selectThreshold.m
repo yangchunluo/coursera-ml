c3fcca131741ddf6d1b63451cd3827d98ba9dabd
Yangchun Luo <yaluo@linkedin.com>
2017-03-03 17:10:13 -0800

    finish ex8 anormaly detection

--- ex8/selectThreshold.m
+++ ex8/selectThreshold.m
@@ -23,17 +23,15 @@ for epsilon = min(pval):stepsize:max(pval)
     % Note: You can use predictions = (pval < epsilon) to get a binary vector
     %       of 0's and 1's of the outlier predictions
 
-
-
-
-
-
-
-
-
-
-
-
+    pred = pval < epsilon;
+    tp = sum(pred == 1 & yval == 1);
+    tn = sum(pred == 0 & yval == 0);
+    fp = sum(pred == 1 & yval == 0);
+    fn = sum(pred == 0 & yval == 1);
+    
+    precision = tp / (tp + fp);
+    recall = tp / (tp + fn);
+    F1 = 2 * precision * recall / (precision + recall);
 
     % =============================================================
 
